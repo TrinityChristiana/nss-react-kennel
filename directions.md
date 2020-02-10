f50197391f2ba42146ca58c49214f0bb4e09c9ee
Trinity Terry <trinitycterry@gmail.com>
2020-02-10 15:22:03 -0600

    added chapter 2

--- directions.md
+++ directions.md
@@ -1,2 +1,3 @@
 # Directions Index
 ## [Chapter 1: React Basics](chapter-1-directions.md)
+## [Chapter 2: React Basics](chapter-2-directions.md)
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
-class LocationCard extends Component {
+class Home extends Component {
 	render() {
 		return (
 			<address>
+				Visit Us at the Nashville North Location
+				<br />
 				500 Puppy Way
 			</address>
 		);
 	}
 }
 
-export default LocationCard;
+export default Home;
