32810133ab8e6414bcdc71845049522666292abb
Trinity Terry <trinitycterry@gmail.com>
2020-02-10 15:43:25 -0600

    added source files

--- src/components/Kennel.jsx
+++ src/components/Kennel.jsx
@@ -1,29 +1,15 @@
 import React, { Component } from 'react';
-import AnimalCard from './animal/AnimalCard';
-import LocationCard from "./location/LocationCard";
-import EmployeeCard from "./employee/EmployeeCard";
-import OwnerCard from './owner/OwnerCard'
 import './Kennel.css';
+import NavBar from './nav/NavBar';
+import ApplicationViews from './ApplicationViews';
 
 class Kennel extends Component {
 	render() {
 		return (
-			<div>
-				<h2>
-					Student Kennels
-					<br />
-					<small>Loving care when you're not there.</small>
-				</h2>
-                <LocationCard />
-                <EmployeeCard />
-                <OwnerCard />
-				
-				<div>
-					<AnimalCard />
-					<AnimalCard />
-					<AnimalCard />
-				</div>
-			</div>
+			<>
+				<NavBar />
+				<ApplicationViews />
+			</>
 		);
 	}
 }
