fbfd4afe08e94fb20f9f1c556d1431d2c90c1491
Trinity Terry <trinitycterry@gmail.com>
2020-02-11 11:33:35 -0600

    fire employees

--- src/components/employee/EmployeeCard.jsx
+++ src/components/employee/EmployeeCard.jsx
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 
 class EmployeeCard extends Component {
 	render() {
-		console.log(this.props)
-
 		return (
 		<div>
 			<h1>{this.props.firstName} {this.props.lastName}</h1>
 			<p>{this.props.employeeNumber}</p>
-			<button type="button" onClick={() => this.props.deleteEmployee(this.props.id)}>Discharge</button>
+			<button type="button" onClick={() => this.props.deleteEmployee(this.props.id)}>Fire!</button>
 		</div>
 		)
 	}
