e702bf573730decbe7ad6b7ae0acd09e41b80f6d
Trinity Terry <trinitycterry@gmail.com>
2020-02-11 11:39:43 -0600

    remove owners

--- src/components/owner/OwnerCard.jsx
+++ src/components/owner/OwnerCard.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 
-class OwnerCard extends Component{
-    render(){
-        return (
-            <div>
-                <h1>Owner: {this.props.firstName} {this.props.lastName}</h1>
-        <p>Pet: {this.props.petName}</p>
-        <p>Phone: {this.props.phoneNumber}</p>
-                
+class OwnerCard extends Component {
+	render() {
+		return (
+			<div>
+				<h1>
+					Owner: {this.props.firstName} {this.props.lastName}
+				</h1>
+				<p>Pet: {this.props.petName}</p>
+				<p>Phone: {this.props.phoneNumber}</p>
+				<button
+					type='button'
+					onClick={() => this.props.removeOwner(this.props.id)}>
+					Remove
+				</button>
 			</div>
-        )
-    }
+		);
+	}
 }
 
 export default OwnerCard;
--- src/components/owner/OwnerList.js
+++ src/components/owner/OwnerList.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 //import the components we will need
 import OwnerCard from './OwnerCard';
-import ownerManager from '../../modules/OwnerManager';
+import OwnerManager from '../../modules/OwnerManager';
 
 class OwnerList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			owners: []
+		};
+	}
 	//define what this component needs to render
-	state = {
-		owners: []
-	};
 
 	componentDidMount() {
 		console.log('Owner LIST: ComponentDidMount');
 		//getAll from AnimalManager and hang on to that data; put it in state
-		ownerManager.getAll().then(owner => {
+		OwnerManager.getAll().then(owner => {
 			this.setState({
 				owners: owner
 			});
 		});
 	}
+
+	removeOwner = id => {
+		OwnerManager.delete(id).then(() => {
+			OwnerManager.getAll().then(owner => {
+				this.setState({
+					owners: owner
+				});
+			})
+		});
+	};
 
 	render() {
 		console.log('Owner LIST: Render');
@@ -25,7 +38,15 @@ class OwnerList extends Component {
 		return (
 			<div className='container-cards'>
 				{this.state.owners.map(owner => (
-					<OwnerCard key={owner.id} firstName={owner.firstName} lastName={owner.lastName} phoneNumber={owner.phoneNumber} petName={owner.petName}/>
+					<OwnerCard
+						key={owner.id}
+						firstName={owner.firstName}
+						lastName={owner.lastName}
+						phoneNumber={owner.phoneNumber}
+						petName={owner.petName}
+						id={owner.id}
+						removeOwner={this.removeOwner}
+					/>
 				))}
 			</div>
 		);
